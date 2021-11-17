d24afcde64b9d80645f12a6a0a57431bf8a65f83
Peter Hui <huishikuen@gmail.com>
2021-11-16 17:58:34 -0800

    updated homepage and repo url

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/HSKPeter/personal-site.git";
+const repoURL = "https://github.com/HSKPeter/hskpeter.github.io.git";
 
 ghpages.publish(
   pathname,
