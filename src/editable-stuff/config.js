7c9a6207827cd753a538002fab0961979ac8a563
Peter Hui <huishikuen@gmail.com>
2021-12-21 18:19:48 -0800

    updated message in the Get In Touch Section

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -51,7 +51,7 @@ const getInTouch = {
   show: true,
   heading: "Get In Touch",
   message:
-    "I'm currently looking for internship opportunities of Summer 2022 (May - Aug) in 🇨🇦Canada. If you know any positions available, or if you have any questions, or if you just want to say hi, please feel free to reach out to me on LinkedIn.",
+    "I'm currently looking for internship Co-op opportunities (May - Dec 2022) in 🇨🇦Canada. If you know any positions available, or if you have any questions, or if you just want to say hi, please feel free to reach out to me on LinkedIn.",
   profileNameOfLinkedIn: "Peter H.",
   linkOfLinkedIn: "https://www.linkedin.com/in/peter-h-84316221b/"
 };
