3290e0c222c91a2af2dcc91cb09dbf1af4d1828e
Peter Hui <huishikuen@gmail.com>
2021-12-31 21:47:16 -0800

    display navbar always

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,39 @@
-import React, { useState } from "react";
-import { useScrollPosition } from "../hooks/useScrollPosition";
-import useResizeObserver from "../hooks/useResizeObserver";
+// import React, { useState } from "react";
+import React from "react";
+// import { useScrollPosition } from "../hooks/useScrollPosition";
+// import useResizeObserver from "../hooks/useResizeObserver";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { mainBody, projects, about, skills } from "../editable-stuff/config.js";
 
-const Navigation = React.forwardRef((props, ref) => {
-  const [isTop, setIsTop] = useState(true);
-  const [scrollPosition, setScrollPosition] = useState(0);
-  const navbarMenuRef = React.useRef();
-  const navbarDimensions = useResizeObserver(navbarMenuRef);
-  const navBottom = navbarDimensions ? navbarDimensions.bottom : 0;
-  useScrollPosition(
-    ({ prevPos, currPos }) => {
-      if (!navbarDimensions) return;
-      currPos.y + ref.current.offsetTop - navbarDimensions.bottom > 5
-        ? setIsTop(true)
-        : setIsTop(false);
-      setScrollPosition(currPos.y);
-    },
-    [navBottom]
-  );
+const Navigation = React.forwardRef(() => {
+  // const [isTop, setIsTop] = useState(true);
+  // const [scrollPosition, setScrollPosition] = useState(0);
+  // const navbarMenuRef = React.useRef();
+  // const navbarDimensions = useResizeObserver(navbarMenuRef);
+  // const navBottom = navbarDimensions ? navbarDimensions.bottom : 0;
+  // useScrollPosition(
+  //   ({ prevPos, currPos }) => {
+  //     if (!navbarDimensions) return;
+  //     currPos.y + ref.current.offsetTop - navbarDimensions.bottom > 5
+  //       ? setIsTop(true)
+  //       : setIsTop(false);
+  //     setScrollPosition(currPos.y);
+  //   },
+  //   [navBottom]
+  // );
 
-  React.useEffect(() => {
-    if (!navbarDimensions) return;
-    navBottom - scrollPosition >= ref.current.offsetTop
-      ? setIsTop(false)
-      : setIsTop(true);
-  }, [navBottom, navbarDimensions, ref, scrollPosition]);
-
+  // React.useEffect(() => {
+  //   if (!navbarDimensions) return;
+  //   navBottom - scrollPosition >= ref.current.offsetTop
+  //     ? setIsTop(false)
+  //     : setIsTop(true);
+  // }, [navBottom, navbarDimensions, ref, scrollPosition]);
+// window.scrollY !== 0 
   return (
     <Navbar
-      ref={navbarMenuRef}
-      className={` fixed-top  ${!isTop ? "navbar-white" : "navbar-transparent"
-        }`}
+      
+      className={` fixed-top navbar-white`}
       expand="lg"
     >
       <Navbar.Brand className="brand" href={process.env.PUBLIC_URL + "/#home"}>
