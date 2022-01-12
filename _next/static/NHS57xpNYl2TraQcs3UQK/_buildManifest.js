3c1a7ec6421075a72e1a60de2fa97bb5fdbf533e
HSKPeter <67510137+HSKPeter@users.noreply.github.com>
2022-01-12 04:03:02 +0000

    Deploying to gh-pages from @ HSKPeter/hskpeter.github.io@2dba7a18762c9228887d03c32539bc207e0defdf 🚀

---
+++ _next/static/NHS57xpNYl2TraQcs3UQK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/192-c058f2db8bd19fdc.js","static/chunks/pages/index-8a0ce8099d0ef9ca.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
